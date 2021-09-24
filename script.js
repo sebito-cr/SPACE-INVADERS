74513ccb2743fbfd8740d2136d4da9fa5e834600
Sebito Liu <74767444+sebito-cr@users.noreply.github.com>
2021-09-24 10:24:02 +0000

    Added Square

--- script.js
+++ script.js
@@ -5,9 +5,48 @@ function setup() {
 
 }
 
+var [xpos, ypos, xspeed, yspeed] = [225, 225, 0, 0];
+
 function draw() {
 	background(225);
-  
-  rect(50,50,20,20);
+	
+	fill(0);
+	text("Use the arrow keys (or WASD) to move the square around", 25, 25);
+	
+	fill(0, 255, 0);
+	rect(xpos, ypos, 50, 50);
+	
+	if(xpos >= 0 && xpos + 50 <= 500) xpos += xspeed;
+	if(ypos >= 0 && ypos + 50 <= 500) ypos += yspeed;
+}
 
-  }
+function keyPressed() {
+	switch(keyCode) {
+		case 37:
+		case 65:
+			xspeed = -2;
+			break;
+		case 39:
+		case 68:
+			xspeed = 2;
+			break;
+		case 38:
+		case 87:
+	}
+}
+
+function keyReleased() {
+	switch(keyCode) {
+		case 37:
+		case 65:
+			xspeed = 0;
+			break;
+		case 39:
+		case 68:
+			xspeed = 0;
+			break;
+		case 38:
+		case 87:
+
+	}
+}
