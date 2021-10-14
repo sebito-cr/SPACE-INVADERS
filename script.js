f60ce574da4e27ce7477345a6ba5ed16ce146bbf
Sebito Liu <74767444+sebito-cr@users.noreply.github.com>
2021-10-14 18:17:27 +0000

    Created Player

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var gameState = 0; // 0 = menu, 1 = game, 2 = gameover
+const CANVAS_WIDTH = 700;
+const CANVAS_HEIGHT = 900;
+let playerImage;
 
 class StartMenu{
   constructor () {}
@@ -6,21 +9,88 @@ class StartMenu{
     background (0);
     textSize (14);
     fill ("white");
-    text ("Use arrow keys to move, space bar to shoot.", 210, 450);
+    textAlign (CENTER);
+    text ("Use arrow keys to move, space bar to shoot.", CANVAS_WIDTH/2, CANVAS_HEIGHT/2);
+  }
+  keyPressed () {
+    switch(keyCode) {	
+    case 32:
+      Game_Screen.startGame ();
+    }
+  }
+  keyReleased () {
+
   }
 }
 
+class Player{ 
+  constructor () {
+    this.xpos = CANVAS_WIDTH/2;
+    this.ypos = CANVAS_HEIGHT-75;
+    this.width = 50;
+    this.height = 30;
+    this.xspeed = 0;
+  }
+  draw () {
+    if (this.xspeed < 0 && this.xpos > -this.xspeed) {
+      this.xpos = this.xpos + this.xspeed;
+    }
+    else if (this.xspeed > 0 && this.xpos < CANVAS_WIDTH-this.width){
+      this.xpos = this.xpos + this.xspeed;
+    }
+    copy(playerImage, 0, 0, 415, 257, this.xpos, this.ypos, this.width, this.height);
+  }
+  keyPressed () {
+    switch(keyCode) {
+		case 37:
+			this.xspeed = -4;
+			break;
+		case 39:
+			this.xspeed = 4;
+			break;		
+	  }
+  }
+  keyReleased () {
+    switch(keyCode) {
+		case 37:
+			this.xspeed = 0;
+			break;
+		case 39:
+			this.xspeed = 0;
+			break;	   
+    }
+  }
+}
+
+let player;
+
 class GameScreen{
   constructor () {}
-  draw () {}
+  draw () {
+    background (0);
+    this.player.draw ();
+  }
+  startGame () {
+    gameState = 1;
+    this.player = new Player ();
+  }
+  keyPressed () {
+    this.player.keyPressed ();
+  }
+  keyReleased () {
+    this.player.keyReleased ();
+  }
 }
 
 let Start_Menu = new StartMenu ();
 let Game_Screen = new GameScreen ();
 
 function setup() {
-	createCanvas(700, 900);
+	createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+}
 
+function preload () {
+  playerImage = loadImage("player.png");
 }
 
 function draw() {
@@ -33,37 +103,21 @@ function draw() {
   }
 }
 
-
 function keyPressed() {
-  console.log(keyCode);
-	switch(keyCode) {
-		case 37:
-		case 65:
-			xspeed = -4;
-			break;
-		case 39:
-		case 68:
-			xspeed = 4;
-			break;		
-    case 32:
-      bulletY = ypos - 12;
-      bulletX = xpos + 30;
-      break;
-	}
+  // console.log (keyCode)
+  if (gameState == 0) {
+    Start_Menu.keyPressed ();
+  }
+  else if ( gameState == 1) {
+    Game_Screen.keyPressed ();
+  }
 }
 
 function keyReleased() {
-	switch(keyCode) {
-		case 37:
-		case 65:
-			xspeed = 0;
-			break;
-		case 39:
-		case 68:
-			xspeed = 0;
-			break;
-		case 38:
-		case 87:
-
-	}
+  if (gameState == 0) {
+    Start_Menu.keyReleased ();
+  }
+  else if (gameState == 1) {
+    Game_Screen.keyReleased ();
+  }
 }
